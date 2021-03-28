e945a371bf0a15ceda3970823982b2961498289b
Tadas <tadasandrijauskass@gmail.com>
2021-03-28 09:29:32 +0300

    App'o kurimo pradzia

--- src/main/java/repository/tables/Client.java
+++ src/main/java/repository/tables/Client.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 @Entity
 public class Client {
+
     private String companyName;
     private Location location;
     private LocalDate jobStart;
---
+++ src/main/java/utils/Project.java
@@ -0,0 +1,49 @@
+package utils;
+
+import java.util.Scanner;
+
+public class Project {
+    Scanner scanner = new Scanner(System.in);
+    int choice;
+
+
+    public void startApp() {
+        while (true) {
+            System.out.println("Sveiki atvykę į įdarbinimo agentūrą\n" +
+                    "1. Darbuotojam.\n" +
+                    "2. Užsakovams\n" +
+                    "0. Iseiti");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    darbuotojam();
+                    break;
+                case 2:
+                    uzsakovam();
+                    break;
+                case 0:
+                    break;
+            }
+        }
+    }
+
+
+    private void darbuotojam() {
+        while (true) {
+            System.out.println("1. Registruoti darbuotoja\n" +
+                    "2. Priimti darbuotoja\n" +
+                    "0. Iseiti");
+            choice = scanner.nextInt();
+        }
+    }
+
+    private void uzsakovam() {
+        while (true) {
+            System.out.println("1. Prideti uzsakyma\n" +
+                    "2. Atsaukti uzsakyma\n" +
+                    "3. Redaguoti uzsakyma\n" +
+                    "0. Iseiti");
+            choice = scanner.nextInt();
+        }
+    }
+}
