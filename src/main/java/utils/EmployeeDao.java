3743564a5a6b633f88f5bad3c798e59e9a303e14
Tadas <tadasandrijauskass@gmail.com>
2021-03-28 09:18:18 +0300

    Darbuotojo kurimo pradzia

--- src/main/java/utils/EmployeeDao.java
+++ src/main/java/utils/EmployeeDao.java
@@ -1,20 +1,8 @@
 package utils;
 
-import hibernate_utils.HibernateUtils;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import repository.tables.Location;
-
 public class EmployeeDao {
 
     private void createEmployee() {
-        SessionFactory sf = HibernateUtils.getSessionFactory(Location.class);
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Location location = new Location();
-        location.setId(10);
-        location.setName("aaa");
-        session.save(location);
-        session.close();
+
     }
 }
