6e50f1f083e1528edcae6f8b5dc5d09af55ad48d
Tadas <tadasandrijauskass@gmail.com>
2021-03-28 09:18:18 +0300

    Darbuotojo kurimo pradzia

--- src/main/java/repository/tables/Client.java
+++ src/main/java/repository/tables/Client.java
@@ -1,18 +1,21 @@
 package repository.tables;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
 public class Client {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String companyName;
     private Location location;
     private LocalDate jobStart;
     private LocalDate jobEnd;
     private Occupation jobs;
-
 
 
 }
--- src/main/java/utils/ClientDao.java
+++ src/main/java/utils/ClientDao.java
@@ -1,12 +1,17 @@
 package utils;
 
+import hibernate_utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import repository.tables.Client;
+
 public class ClientDao {
 
     public void addClient() {
-//        SessionFactory sf = HibernateUtils.getSessionFactory(Client.class);
-//        Session session = sf.openSession();
-//        session.beginTransaction();
-//        session.save(new Client());
+        SessionFactory sf = HibernateUtils.getSessionFactory(Client.class);
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(new Client());
 
     }
 
