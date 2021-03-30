560c2648133d42c5098618fe69a7cd994304f174
Gediminas <ged.jak@gmail.com>
2021-03-30 14:24:49 +0300

    sukurtos visos ClientDao funkcijos, liko pritaikyti exeption kai įvedami netinkami duomenys

--- src/main/java/repository/tables/Client.java
+++ src/main/java/repository/tables/Client.java
@@ -12,10 +12,90 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String companyName;
-    private Location location;
+    private String location;
     private LocalDate jobStart;
     private LocalDate jobEnd;
-    private Occupation jobs;
-// pakeitimas
+    private String occupation;
+    private double hourlyRate;
 
+    public Client() {
+    }
+
+    public Client(String companyName, String location, LocalDate jobStart, LocalDate jobEnd, String occupation, double hourlyRate) {
+        this.companyName = companyName;
+        this.location = location;
+        this.jobStart = jobStart;
+        this.jobEnd = jobEnd;
+        this.occupation = occupation;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", location='" + location + '\'' +
+                ", jobStart=" + jobStart +
+                ", jobEnd=" + jobEnd +
+                ", occupation='" + occupation + '\'' +
+                ", hourlyRate=" + hourlyRate +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDate getJobStart() {
+        return jobStart;
+    }
+
+    public void setJobStart(LocalDate jobStart) {
+        this.jobStart = jobStart;
+    }
+
+    public LocalDate getJobEnd() {
+        return jobEnd;
+    }
+
+    public void setJobEnd(LocalDate jobEnd) {
+        this.jobEnd = jobEnd;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
 }
